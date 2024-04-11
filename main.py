36090071168bcee6f88dcbb62800b0330b19e399
Vinay Pant <vinaypant24@gmail.com>
2024-04-11 22:39:45 +0530

    Skeleton Code - First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+
+
+
+
+class MainApp(MDApp):
+
+    def build(self):
+        screen  = Screen
+
+        return None
+    
+
+
+
+if __name__ == '__main__':
+        MainApp().run()
